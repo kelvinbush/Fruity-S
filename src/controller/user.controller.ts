98bd12994f1de4f227741fb238da29fde2fd26e2
Kelvin Bush <kelybush@gmail.com>
2022-09-02 06:46:07 +0300

    fix send status

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -9,20 +9,18 @@ export async function getCurrentUser(req: Request, res: Response) {
 		res.send({ user });
 		return;
 	} else {
-		res.send({ message: "Failed to get User" }).sendStatus(500);
+		res.sendStatus(500);
 		return;
 	}
 }
 
 export async function updateUserAddress(req: Request, res: Response) {
-	logger.info("Controller");
-	logger.info(req.body);
 	const userId = res.locals.user.uid;
 	const user = await updateAddress(userId, req.body);
 	if (user) {
 		res.send({ message: "User Address updated successfully" });
 		return;
 	}
-	res.send({ message: "Failed to update User" }).sendStatus(500);
+	res.sendStatus(500);
 	return;
 }
